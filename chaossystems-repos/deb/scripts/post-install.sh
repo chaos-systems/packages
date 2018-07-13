07b27deecf61792b8fe4a676b8bbc95d71039ea6
Greg Orzell <gorzell@github.com>
2018-07-13 11:00:22 +0200

    Add a fonts package and clean up some stuff that should have never been checked in.

--- chaossystems-repos/deb/scripts/post-install.sh
+++ chaossystems-repos/deb/scripts/post-install.sh
@@ -9,6 +9,11 @@ dpkg --add-architecture i386
 echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
 
+# i3
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
+dpkg -i ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
@@ -17,4 +22,4 @@ add-apt-repository \
    stable"
 
 # Nodejs
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
+# curl -sL https://deb.nodesource.com/setup_10.x | bash -
