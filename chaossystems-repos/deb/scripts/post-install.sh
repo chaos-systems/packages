e093261dfeefff2e75c89557bf1e850af6f040f4
Greg Orzell <gorzell@github.com>
2018-10-01 20:43:13 +0200

    Restructure how additonal repos are added.

--- chaossystems-repos/deb/scripts/post-install.sh
+++ chaossystems-repos/deb/scripts/post-install.sh
@@ -5,21 +5,40 @@ dpkg --add-architecture i386
 
 # Additional repos
 
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Azul
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
+apt-add-repository "deb http://repos.azulsystems.com/ubuntu stable main"
+
 # SBT
-echo "deb https://dl.bintray.com/sbt/debian /" | tee /etc/apt/sources.list.d/sbt.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+add-apt-repository "deb https://dl.bintray.com/sbt/debian /"
+
+# Google Cloud
+curl -sL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-add-repository "deb http://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -cs) main"
+
+# Kubernetes
+curl -sL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+# Atom
+curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+apt-add-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
+
+# VS Code
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+apt-add-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+# Slack
+curl -sL https://packagecloud.io/slacktechnologies/slack/gpgkey | apt-key add -
+apt-add-repository "deb https://packagecloud.io/slacktechnologies/slack/debian/ jessie main"
 
 # i3
 /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
-dpkg -i ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
-
-# Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-# Nodejs
-# curl -sL https://deb.nodesource.com/setup_10.x | bash -
+#dpkg -i ./keyring.deb
+add-apt-repository "deb http://debian.sur5r.net/i3/ $(lsb_release -cs) universe"
