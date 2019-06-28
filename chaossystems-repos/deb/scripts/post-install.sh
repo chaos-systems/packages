9d92c1e9df0c5f97d919a360378dc05f073b87e2
Greg Orzell <gorzell@github.com>
2019-06-28 14:31:34 +0200

    Some general cleanup and the start of a re-work for the repositories package.

--- chaossystems-repos/deb/scripts/post-install.sh
+++ chaossystems-repos/deb/scripts/post-install.sh
@@ -3,23 +3,17 @@
 # Enable i386 for ironkey
 dpkg --add-architecture i386
 
-# Additional repos
+# Additional repos' keys
 
 # SBT
-echo "deb https://dl.bintray.com/sbt/debian /" | tee /etc/apt/sources.list.d/sbt.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
 
 # i3
-/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
 dpkg -i ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
 
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
 
 # Nodejs
 # curl -sL https://deb.nodesource.com/setup_10.x | bash -
