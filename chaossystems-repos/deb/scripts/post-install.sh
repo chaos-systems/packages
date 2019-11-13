2025d36b984e5cee279b34ee48b9bed58a90489b
Greg Orzell <gorzell@github.com>
2019-11-13 20:38:17 +0100

    Switch to gradle an a bunch of Docker helpers.

--- chaossystems-repos/deb/scripts/post-install.sh
+++ chaossystems-repos/deb/scripts/post-install.sh
@@ -4,16 +4,5 @@
 dpkg --add-architecture i386
 
 # Additional repos' keys
-
-# SBT
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-
-# i3
-/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
-dpkg -i ./keyring.deb
-
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-# Nodejs
-# curl -sL https://deb.nodesource.com/setup_10.x | bash -
